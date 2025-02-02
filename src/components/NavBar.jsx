0d73514b2a45336072f68b89d68740c333e5ec54
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2025-02-02 19:48:15 +1100

    Add cart item coutner to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.webp'
 import '../styles/NavBar.css'
+import { useContext } from 'react'
+import ShopContext from '../contexts/ShopContext'
 
 const NavBar = () => {
+    const { cartItems } = useContext(ShopContext);
+
     return (
         <div id="nav-bar">
             <Link to="/">
@@ -12,7 +16,7 @@ const NavBar = () => {
                 <ul>
                     <li><Link to="/">Homepage</Link></li>
                     <li><Link to="/Shopping">Shopping</Link></li>
-                    <li><Link to="/Cart">Cart</Link></li>
+                    <li><Link to="/Cart">Cart{cartItems.length > 0 ? <span className="numItems"> #{cartItems.length}</span> : null}</Link></li>
                 </ul>
             </nav>
         </div>
